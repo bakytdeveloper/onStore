b37fe8a16e38ea46ddc8afaa5c4e72f62c56125e
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-21 14:11:11 +0600

    Создал шапку на фронтэнде

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import ProductList from "./components/ProductList/ProductList";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+// const Home = () => <div>Home</div>;
+
 function App() {
+
+  // Предположим, у вас есть стейт для хранения списка товаров
+  const [products, setProducts] = useState([]);
+
+  // Используйте useEffect для получения данных с сервера
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('http://localhost:5500/api/products');
+        const data = await response.json();
+        setProducts(data);
+        console.log(data)
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []); // Пустой массив зависимостей гарантирует вызов useEffect только после монтирования компонента
+
+  const [selectedDirection, setSelectedDirection] = useState(null);
+
+  const handleDirectionSelect = (direction) => {
+    setSelectedDirection(direction);
+  };
+
+  const handleProductsLoad = (loadedProducts) => {
+    setProducts(loadedProducts);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div>
+          <Header />
+          <Sidebar
+              onDirectionSelect={handleDirectionSelect}
+              onProductsLoad={handleProductsLoad}
+          />
+          <Switch>
+
+
+            <ProductList products={products} />
+
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
 export default App;
+
+

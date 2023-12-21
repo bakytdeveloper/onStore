518a5691a05870377e071ffa869ae5f5bac335cf
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-21 13:27:17 +0600

    Веду подготовку бэкэнда

---
+++ models/Product.js
@@ -0,0 +1,45 @@
+// server/models/Product.js
+const mongoose = require('mongoose');
+
+const characteristicSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: String, required: true },
+});
+
+const productSchema = new mongoose.Schema({
+    direction: { type: String, required: true },
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    images: [{ type: String }],
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    characteristics: [characteristicSchema],
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
+
+
+
+
+// {
+//     "direction": "Гаджет",
+//     "type": "iPhone",
+//     "brand": "Apple",
+//     "images": ["https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/61jLiCovxVL._AC_SX679_.jpg",
+//     "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/71zwao302EL._AC_SX679_.jpg",
+//     "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81DzfVDR-lL._AC_SX679_.jpg",
+//     "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/7161nwSVX9L._AC_SX679_.jpg",
+//     "https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/81--JfmMOIL._AC_SX679_.jpg"],
+//     "name": "iPhone 13 Pro",
+//     "description": "iPhone 13 Pro, 256 ГБ, графитовый — разблокирован (обновленный премиум-версия).",
+//     "price": 709.99,
+//     "characteristics": [
+//     { "name": "Беспроводной оператор", "value": "Разблокировано для всех операторов связи" },
+//     { "name": "Операционная система", "value": "iOS 16" },
+//     { "name": "Клеточные технологии", "value": "5G" },
+//     { "name": "Емкость памяти", "value": "256 ГБ" }
+// ]
+// }
---
+++ models/User.js
@@ -0,0 +1,19 @@
+// server/models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['client', 'guest', 'admin'], default: 'guest' },
+    cart: [{
+        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        quantity: { type: Number, default: 1 },
+    }],
+    purchaseHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
+    firstName: { type: String },
+    lastName: { type: String },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;

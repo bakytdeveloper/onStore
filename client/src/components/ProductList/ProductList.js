6450e6e45c61fdba98af61c51d1932232f051997
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-23 15:36:52 +0600

    Поправил код , удалил все лишнее

--- client/src/components/ProductList/ProductList.js
+++ client/src/components/ProductList/ProductList.js
@@ -1,30 +1,3 @@
-// // client/src/components/ProductList/ProductList.js
-// import React from 'react';
-// import ProductCard from './../ProductCard/ProductCard';
-// import './ProductList.css';
-// import {Link} from "react-router-dom";
-//
-// const ProductList = ({ products }) => {
-//     return (
-//         <div className="product-list">
-//             {products.map((product, index) => (
-//                 <Link key={product._id}  to={`/details/${product._id}`}>
-//                 <ProductCard
-//                     className="product-card"
-//                     key={product._id}
-//                     images={product.images || []}
-//                     brand={product.brand}
-//                     description={product.description}
-//                     price={product.price}
-//                 />
-//                 </Link>
-//             ))}
-//         </div>
-//     );
-// };
-//
-// export default ProductList;
-
 
 
 
